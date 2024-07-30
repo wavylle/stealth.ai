85264efbbb97f779ec65c5753d147da0fb9550ba
Abhirath Dubey <abhirathdubey2804@gmail.com>
2024-07-31 00:59:05 +0530

    auth routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
         ) : (
           <div className="flex gap-4 items-center">
             <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-              <Link href="/sign-up">Sign up</Link>
+              <Link href="/auth/sign-up">Sign up</Link>
             </div>
             <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-              <Link href="/sign-in">Sign in</Link>
+              <Link href="/auth/sign-in">Sign in</Link>
             </div>
           </div>
         )}
