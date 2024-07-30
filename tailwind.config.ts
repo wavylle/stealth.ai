7fbc59e8c54099df2162564314742a8b770ab38d
pavanxs <83165442+pavanxs@users.noreply.github.com>
2024-07-30 15:24:44 +0530

    Tailwind commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,7 @@ const config = {
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
