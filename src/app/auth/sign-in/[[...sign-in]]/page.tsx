a5a0ff4a8418a7f7efc2e688035569e80a7cfe21
Abhirath Dubey <abhirathdubey2804@gmail.com>
2024-08-03 21:09:40 +0530

    auth pages edit

--- src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,28 @@
-import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+import { ClerkLoaded, ClerkLoading, SignIn } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center flex-col gap-10 mt-20">
-      {/* <h1 className="text-4xl font-bold mt-20">This is signup page</h1> */}
-      <SignIn />
+  <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+  <div className="h-full lg:flex flex-col items-center justify-center">
+    <div className="text-center space-y-4 pt-16">
+      {/* <h1 className="font-bold text-3xl text-[#2E2A47]">
+        Welcome Back!
+      </h1> */}
     </div>
+    <div className="flex items-center justify-center">
+      <ClerkLoaded>
+        <SignIn  />
+      </ClerkLoaded>
+      <ClerkLoading>
+        <Loader2 className="animate-spin text-muted-foreground" />
+      </ClerkLoading>
+    </div>
+  </div>
+  <div className="h-full bg-indigo-400 hidden lg:flex items-center justify-center">
+    <Image src="/testlogo.svg" height={200} width={200} alt="logo"/>
+  </div>
+</div>
   );
 }
