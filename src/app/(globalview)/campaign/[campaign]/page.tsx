65ea413736c92dfd04caf68b58a50f41842c1150
pavanxs <83165442+pavanxs@users.noreply.github.com>
2024-08-07 15:40:16 +0530

    tr

---
+++ src/app/(globalview)/campaign/[campaign]/page.tsx
@@ -0,0 +1,7 @@
+export default function CampaignPage() {
+    return( 
+        <div>
+            individual campaign page
+        </div>
+    )
+}
---
+++ src/app/(globalview)/campaign/page.tsx
@@ -0,0 +1,7 @@
+export default function CampaignPage() {
+    return( 
+        <div>
+            IDK whats this for
+        </div>
+    )
+}
